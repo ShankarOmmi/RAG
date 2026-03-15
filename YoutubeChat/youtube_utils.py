a410d7bdd7c333b2efb764c45d6fb6971b76f95c
ShankarOmmi <balashankar1437@gmail.com>
2026-03-15 14:31:21 +0530

    Modified YoutubeUtilities for cloud deployemeny

--- YoutubeChat/youtube_utils.py
+++ YoutubeChat/youtube_utils.py
@@ -1,38 +1,32 @@
+from langchain_community.document_loaders import YoutubeLoader
 from urllib.parse import urlparse, parse_qs
-from youtube_transcript_api import YouTubeTranscriptApi
 
-from urllib.parse import urlparse, parse_qs
 
 def get_video_id(url):
 
-    parsed_url = urlparse(url)
+    parsed = urlparse(url)
 
-    # Case 1: youtu.be short link
-    if parsed_url.hostname == "youtu.be":
-        return parsed_url.path[1:]
+    if parsed.hostname == "youtu.be":
+        return parsed.path[1:]
 
-    # Case 2: youtube.com/watch?v=
-    if parsed_url.hostname in ("www.youtube.com", "youtube.com"):
-        if parsed_url.path == "/watch":
-            return parse_qs(parsed_url.query)["v"][0]
-
-        # Case 3: youtube.com/shorts/VIDEO_ID
-        if parsed_url.path.startswith("/shorts/"):
-            return parsed_url.path.split("/")[2]
-
-        # Case 4: youtube.com/embed/VIDEO_ID
-        if parsed_url.path.startswith("/embed/"):
-            return parsed_url.path.split("/")[2]
+    if parsed.hostname in ("www.youtube.com", "youtube.com"):
+        if parsed.path == "/watch":
+            return parse_qs(parsed.query)["v"][0]
 
     raise ValueError("Invalid YouTube URL")
 
-    
+
 def fetch_transcript(url):
 
     video_id = get_video_id(url)
 
-    transcript_list = YouTubeTranscriptApi().fetch(video_id)
+    loader = YoutubeLoader.from_youtube_url(
+        f"https://www.youtube.com/watch?v={video_id}",
+        add_video_info=False
+    )
 
-    transcript = " ".join(chunk.text for chunk in transcript_list)
+    docs = loader.load()
+
+    transcript = " ".join(doc.page_content for doc in docs)
 
     return transcript
